48b1c189c026575bed7b19723248bc11965eeba8
Franegeaa <133876124+Franegeaa@users.noreply.github.com>
2024-09-17 18:47:24 -0300

    Añade estilos a los botones main del formulario

--- TrabajosPracticos/Tp6/publicar-pedido-envio/src/data/mockTransportistas.js
+++ TrabajosPracticos/Tp6/publicar-pedido-envio/src/data/mockTransportistas.js
@@ -3,38 +3,38 @@ const transportistas = [
     id: 1,
     nombre: "Luca",
     mail: "spaccesi.luca@gmail.com",
-    zonaCobertura: ["Buenos Aires", "La Plata", "Morón"]
+    zonaCobertura: ["Buenos Aires", "La Plata", "Morón"],
   },
   {
     id: 2,
     nombre: "Mateo",
     mail: "matevarela32@gmail.com",
-    zonaCobertura: ["Rosario", "Córdoba", "San Nicolás"]
+    zonaCobertura: ["Rosario", "Córdoba", "San Nicolás"],
   },
   {
     id: 3,
     nombre: "Irichbro",
-    mail: "irichbro@gmail.com",
-    zonaCobertura: ["Tigre", "Escobar", "Zárate"]
+    mail: "egeafran02@gmail.com",
+    zonaCobertura: ["Tigre", "Escobar", "Zárate"],
   },
   {
     id: 4,
     nombre: "Transportista 4",
     mail: "transportista4@example.com",
-    zonaCobertura: ["La Plata", "Mar del Plata", "Chascomús"]
+    zonaCobertura: ["La Plata", "Mar del Plata", "Chascomús"],
   },
   {
     id: 5,
     nombre: "Transportista 5",
     mail: "transportista5@example.com",
-    zonaCobertura: ["San Isidro", "Tigre", "Vicente López"]
+    zonaCobertura: ["San Isidro", "Tigre", "Vicente López"],
   },
   {
     id: 6,
     nombre: "Transportista 6",
     mail: "transportista6@example.com",
-    zonaCobertura: ["Córdoba", "Villa Carlos Paz", "Alta Gracia"]
-  }
+    zonaCobertura: ["Córdoba", "Villa Carlos Paz", "Alta Gracia"],
+  },
 ];
 
 export default transportistas;
